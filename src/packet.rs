6598cc28991569d5810db267971c7daa3dfd5f69
Aaron Kofsky <aaronko@umich.edu>
2023-01-07 14:51:52 -0500

    Fix out of bounds crash in parsing the security envelope.
    Previously, if the slice containing the security envelope is too short,
    we could easily try and access out of bounds indicies and crash. This
    commit fixes that by doing bounds checking. If there isn't enough data,
    the parser returns an Err instead of crashing.

--- src/packet.rs
+++ src/packet.rs
@@ -1,17 +1,21 @@
-use thrift::{
-    protocol::{TBinaryInputProtocol, TBinaryOutputProtocol, TSerializable},
-    transport::ReadHalf,
-};
+use crate::models::encoding::PROTOCOL_MAJOR_VERSION;
 
-use crate::models::encoding::{ProtocolPacket, PROTOCOL_MAJOR_VERSION};
-
-fn parse_packet(
+// Parse a ProtocolPacket contained in a security envelope.
+// The returned tuple consists of three things. First is the header of the outer security envelope.
+// If a TIE Origin security envelope present, that is also returned. Finally, the unconsumed
+// portion of the input (which should correspond to the start of the raw `ProtocolPacket` data)
+// is returned.
+// Note that the contents of the security fingerprint are not verified--the only checking done here
+// is that the data starts with the correct magic bytes, the major version matches, and that all of
+// the fields are actually present. Additionally, the `ProtocolPacket` data itself is unparsed and
+// may be invalid.
+pub fn parse_security_envelope(
     bytes: &[u8],
 ) -> Result<
     (
         OuterSecurityEnvelopeHeader,
         Option<TIEOriginSecurityEnvelopeHeader>,
-        ProtocolPacket,
+        &[u8],
     ),
     ParsingError,
 > {
@@ -24,28 +28,19 @@ fn parse_packet(
             let (header, bytes) = TIEOriginSecurityEnvelopeHeader::parse_packet(bytes)?;
             (Some(header), bytes)
         };
-
-    // TODO: Should this be in "strict mode"?
-    let mut binary_protocol = TBinaryInputProtocol::new(ReadHalf::new(bytes), true);
-    let protocol_packet = ProtocolPacket::read_from_in_protocol(&mut binary_protocol)
-        .map_err(ParsingError::ThriftError)?;
-
-    Ok((
-        outer_security_header,
-        tie_origin_security_header,
-        protocol_packet,
-    ))
+    Ok((outer_security_header, tie_origin_security_header, bytes))
 }
 
-struct OuterSecurityEnvelopeHeader<'a> {
-    packet_number: u16,
-    major_version: u8,
-    outer_key_id: u8,
-    fingerprint_length: u8,
-    security_fingerprint: &'a [u8],
-    weak_nonce_local: u16,
-    weak_nonce_remote: u16,
-    remaining_tie_lifetime: u32,
+#[derive(Debug)]
+pub struct OuterSecurityEnvelopeHeader<'a> {
+    pub packet_number: u16,
+    pub major_version: u8,
+    pub outer_key_id: u8,
+    pub fingerprint_length: u8,
+    pub security_fingerprint: &'a [u8],
+    pub weak_nonce_local: u16,
+    pub weak_nonce_remote: u16,
+    pub remaining_tie_lifetime: u32,
 }
 
 impl<'a> OuterSecurityEnvelopeHeader<'a> {
@@ -53,38 +48,32 @@ impl<'a> OuterSecurityEnvelopeHeader<'a> {
         bytes: &'a [u8],
     ) -> Result<(OuterSecurityEnvelopeHeader<'a>, &'a [u8]), ParsingError> {
         // Check RIFT_MAGIC bytes (RIFT_MAGIC value is expected to equal 0xA1F7)
-        let rift_magic = u16::from_be_bytes([bytes[0], bytes[1]]);
+        let rift_magic = get_u16(bytes, 0)?;
         if rift_magic != 0xA1F7 {
             return Err(ParsingError::NotMagical);
         }
 
-        let packet_number = u16::from_be_bytes([bytes[2], bytes[3]]);
+        let packet_number = get_u16(bytes, 2)?;
 
-        let _reserved = bytes[4];
+        let _reserved = get_u8(bytes, 4)?;
 
-        let major_version = bytes[5];
+        let major_version = get_u8(bytes, 5)?;
         if major_version != PROTOCOL_MAJOR_VERSION as u8 {
             return Err(ParsingError::WrongMajorVersion);
         }
 
-        let outer_key_id = bytes[6];
-        let fingerprint_length = bytes[7];
+        let outer_key_id = get_u8(bytes, 6)?;
 
+        let fingerprint_length = get_u8(bytes, 7)?;
         let fingerprint_end = 8 + fingerprint_length as usize * 4;
-        let security_fingerprint = &bytes[8..fingerprint_end];
 
-        let weak_nonce_local =
-            u16::from_be_bytes([bytes[fingerprint_end], bytes[fingerprint_end + 1]]);
+        let security_fingerprint = &bytes
+            .get(8..fingerprint_end)
+            .ok_or(ParsingError::OutOfRange)?;
 
-        let weak_nonce_remote =
-            u16::from_be_bytes([bytes[fingerprint_end + 2], bytes[fingerprint_end + 3]]);
-
-        let remaining_tie_lifetime = u32::from_be_bytes([
-            bytes[fingerprint_end + 4],
-            bytes[fingerprint_end + 5],
-            bytes[fingerprint_end + 6],
-            bytes[fingerprint_end + 7],
-        ]);
+        let weak_nonce_local = get_u16(bytes, fingerprint_end)?;
+        let weak_nonce_remote = get_u16(bytes, fingerprint_end + 2)?;
+        let remaining_tie_lifetime = get_u32(bytes, fingerprint_end + 4)?;
 
         let header = OuterSecurityEnvelopeHeader {
             packet_number,
@@ -101,21 +90,29 @@ impl<'a> OuterSecurityEnvelopeHeader<'a> {
     }
 }
 
-struct TIEOriginSecurityEnvelopeHeader<'a> {
-    tie_origin_key_id: u32, // this is actually only 24 bits long
-    fingerprint_length: u8,
-    security_fingerprint: &'a [u8],
+#[derive(Debug)]
+pub struct TIEOriginSecurityEnvelopeHeader<'a> {
+    pub tie_origin_key_id: u32, // this is actually only 24 bits long
+    pub fingerprint_length: u8,
+    pub security_fingerprint: &'a [u8],
 }
 
 impl<'a> TIEOriginSecurityEnvelopeHeader<'a> {
     fn parse_packet(
         bytes: &'a [u8],
     ) -> Result<(TIEOriginSecurityEnvelopeHeader<'a>, &'a [u8]), ParsingError> {
-        let tie_origin_key_id = u32::from_be_bytes([bytes[0], bytes[1], bytes[2], 0]);
-        let fingerprint_length = bytes[3];
+        let tie_origin_key_id = {
+            let b0 = *bytes.get(0).ok_or(ParsingError::OutOfRange)?;
+            let b1 = *bytes.get(1).ok_or(ParsingError::OutOfRange)?;
+            let b2 = *bytes.get(2).ok_or(ParsingError::OutOfRange)?;
+            u32::from_be_bytes([b0, b1, b2, 0])
+        };
+        let fingerprint_length = get_u8(bytes, 3)?;
 
         let fingerprint_end = 4 + fingerprint_length as usize * 4;
-        let security_fingerprint = &bytes[4..fingerprint_end];
+        let security_fingerprint = bytes
+            .get(4..fingerprint_end)
+            .ok_or(ParsingError::OutOfRange)?;
 
         let header = TIEOriginSecurityEnvelopeHeader {
             tie_origin_key_id,
@@ -126,8 +123,30 @@ impl<'a> TIEOriginSecurityEnvelopeHeader<'a> {
     }
 }
 
-enum ParsingError {
+fn get_u8(slice: &[u8], index: usize) -> Result<u8, ParsingError> {
+    let b0 = slice.get(index).ok_or(ParsingError::OutOfRange)?;
+    Ok(*b0)
+}
+
+fn get_u16(slice: &[u8], index: usize) -> Result<u16, ParsingError> {
+    let b0 = slice.get(index).ok_or(ParsingError::OutOfRange)?;
+    let b1 = slice.get(index + 1).ok_or(ParsingError::OutOfRange)?;
+    Ok(u16::from_be_bytes([*b0, *b1]))
+}
+
+fn get_u32(slice: &[u8], index: usize) -> Result<u32, ParsingError> {
+    let b0 = slice.get(index).ok_or(ParsingError::OutOfRange)?;
+    let b1 = slice.get(index + 1).ok_or(ParsingError::OutOfRange)?;
+    let b2 = slice.get(index + 2).ok_or(ParsingError::OutOfRange)?;
+    let b3 = slice.get(index + 3).ok_or(ParsingError::OutOfRange)?;
+
+    Ok(u32::from_be_bytes([*b0, *b1, *b2, *b3]))
+}
+
+#[derive(Debug)]
+pub enum ParsingError {
     NotMagical,
     WrongMajorVersion,
     ThriftError(thrift::Error),
+    OutOfRange,
 }
